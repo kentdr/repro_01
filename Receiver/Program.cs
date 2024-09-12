2693d4bd152522c64e96296aa3f203bd6faffff1
Dan Kent <dan.kent@particular.net>
2024-09-12 15:39:01 -0400

    Bump Receiver to AmazonSQS 6.2.0. Remove Sql Persistence

--- Receiver/Program.cs
+++ Receiver/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Threading.Tasks;
-using Microsoft.Data.SqlClient;
 using NServiceBus;
 
 
@@ -10,20 +9,12 @@ class Program
     {
         Console.Title = "SimpleReceiver";
         var endpointConfiguration = new EndpointConfiguration("Drk.Samples.Sqs.SimpleReceiver");
+        endpointConfiguration.UseSerialization<XmlSerializer>();
+
         endpointConfiguration.EnableInstallers();
         var transport = endpointConfiguration.UseTransport<SqsTransport>();
         transport.S3("drk.bucketname", "drk");
         transport.QueueNamePrefix("drk");
-
-        var sub = transport.EnableMessageDrivenPubSubCompatibilityMode();
-        sub.RegisterPublisher(typeof(MyEvent), "Drk.Samples.Sqs.SimpleReceiver");
-
-        var p = endpointConfiguration.UsePersistence<SqlPersistence>();
-        p.ConnectionBuilder(() => new SqlConnection(@"Server=localhost,1433;Initial Catalog=NsbSamplesSqlPersistence;User Id=SA;Password=yourStrong(!)Password;Encrypt=false"));
-        p.SqlDialect<SqlDialect.MsSqlServer>();
-        p.SqlDialect<SqlDialect.MsSqlServer>();
-        var subscriptions = p.SubscriptionSettings();
-        subscriptions.CacheFor(TimeSpan.FromMinutes(5));
 
         var endpointInstance = await Endpoint.Start(endpointConfiguration);
         Console.WriteLine("Press any key to exit");
